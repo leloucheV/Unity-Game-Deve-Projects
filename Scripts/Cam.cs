41e3ec9b6926b07808c6b27b2ae26de708b1a13e
leloucheV <lr2327912@gmail.com>
2017-01-19 02:19:41 +0500

    Further changes will be applies, this is just an experiment. Will expand ballroller.
    Gives the basic overview of ball roller, will include scripts and assets. That have been studied thoroughly. The main exe file is included so that one may be able to play the game.

---
+++ Scripts/Cam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cam : MonoBehaviour {
+    public GameObject player;
+    public Vector3 TrackOffset;
+	// Use this for initialization
+	void Start () {
+
+        TrackOffset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+    //late update is called after the usual update, per frame
+    //for follow cameras, procedural animation AND FOR GATHERING LAST KNOWN STATES, its best to use lateUpdate
+	void LateUpdate () {
+
+        transform.position = player.transform.position + TrackOffset;
+	}
+}
---
+++ Scripts/Movement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour {
+    private Rigidbody rb;
+    public float Speed;
+    public Text countText;
+    private int count;
+    public Text winText;
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        //always remember that this is where physics calculations are made before execution in update
+
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalMovement, 0, verticalMovement);
+        rb.AddForce(movement*Speed);
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+        }
+
+       
+    }
+    void setCountText()
+    {
+        countText.text = "Points:" + count.ToString();
+        if (count == 7)
+        {
+            winText.text = "You have won!";
+        }
+    }
+}
